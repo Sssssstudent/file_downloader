8b10d5daa51e4d1c0b60f8d62e503881486b8752
Alexandr Volnov <DEDAN32092yandex.ru>
2019-04-21 21:20:23 +0300

    Add a basic controller for starting page, add a templates for response on url "/"

---
+++ src/main/java/ru/bellintegrator/controller/Main.java
@@ -0,0 +1,14 @@
+package ru.bellintegrator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class Main {
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model){
+        return "greeting";
+    }
+}
---
+++ src/main/resources/templates/greeting.ftl
@@ -0,0 +1,5 @@
+<#import "parts/common.ftl" as c>
+<@c.page>
+    <h5>Hellow, new User!</h5>
+    <div>It's file-downloader service.</div>
+</@c.page>
